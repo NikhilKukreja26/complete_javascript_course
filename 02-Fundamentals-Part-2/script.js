949dbdba52eaf92992e427e157cfb4d36312d125
NikhilKukreja26 <kukrejanikhil25@gmail.com>
2024-07-20 15:29:24 +0530

    - Function declaration and expression

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+/*
 let hasDriversLicense = false;
 const passTest = true;
 
@@ -8,3 +9,44 @@ if (hasDriversLicense) console.log('I can drive :D');
 
 // const interface = 'Audio';
 // const private = 534;
+
+
+function logger() {
+  console.log('My name is Nikhil');
+}
+
+// calling / running / invoking function
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+  const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+  return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
+
+const num = Number('5');
+console.log(num);
+*/
+
+// Function declaration
+function calcAge1(birthYear) {
+  return 2037 - birthYear;
+}
+
+const age1 = calcAge1(1996);
+
+// Function expression
+
+const calcAge2 = function (birthYear) {
+  return 2037 - birthYear;
+};
+
+const age2 = calcAge2(1996);
+console.log(age1, age2);
