3312192dba45dc2fe0f98ce3ee2fddc1a6a8c86a
NikhilKukreja26 <kukrejanikhil25@gmail.com>
2024-08-10 14:02:21 +0530

    - While loop

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -328,7 +328,6 @@ for (let i = 0; i < nikhil.length; i++) {
 
   console.log(nikhil[i], typeof nikhil[i]);
 }
-*/
 
 const nikhil = [
   'Nikhil',
@@ -348,4 +347,24 @@ for (let exercise = 1; exercise < 4; exercise++) {
   for (let rep = 1; rep < 6; rep++) {
     console.log(`Exercise ${exercise}: Lifting weight repetition ${rep} 🏋️‍♀️`);
   }
+}
+*/
+
+// for (let rep = 1; rep <= 10; rep++) {
+//   console.log(`Lifting weights repetition ${rep} 🏋️‍♀️`);
+// }
+
+let rep = 1;
+
+while (rep <= 10) {
+  // console.log(`WHILE: Lifting weights repetition ${rep} 🏋️‍♀️`);
+  rep++;
+}
+
+let dice = Math.trunc(Math.random() * 6) + 1;
+
+while (dice !== 6) {
+  console.log(`You rolled a ${dice}`);
+  dice = Math.trunc(Math.random() * 6) + 1;
+  if (dice === 6) console.log('Loop is about to end...');
 }
